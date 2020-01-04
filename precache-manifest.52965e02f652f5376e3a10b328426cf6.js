cb5359382dd5499ba20d157fd6148fb39502634d
Lonewolf5432 <bilsonc47@gmail.com>
2020-01-04 18:58:25 +0545

    Updates

---
+++ precache-manifest.52965e02f652f5376e3a10b328426cf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696fc7be765ac5fd4bf8a7a3f7e95471",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "61d196b564db29a3f748",
+    "url": "/todolist-react/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "61d196b564db29a3f748",
+    "url": "/todolist-react/static/js/2.24363a1a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolist-react/static/js/2.24363a1a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2bd70346646e79aec548",
+    "url": "/todolist-react/static/js/main.ffea2f7b.chunk.js"
+  },
+  {
+    "revision": "4a3a13ab17e4eeb2107c",
+    "url": "/todolist-react/static/js/runtime-main.4553e59f.js"
+  }
+]);
